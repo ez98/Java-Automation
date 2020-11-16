73b9b3d251b1c02bec96637b9afa1df416c7d421
ezaca <ericzacarias80@gmail.com>
2020-11-16 13:03:07 -0600

    PS5

--- JavaAutomation/src/automate/AutomateTest.java
+++ JavaAutomation/src/automate/AutomateTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 /*
- * Unfinished automation script to purchase Playstation 5
+ * Unfinished automation script to purchase Playstation 5.
  */
 public class AutomateTest {
 
